c479a82f5fd5a20d3e9397e7b7259fc932e41059
Martin Skalák <martin.skalak02@gmail.com>
2021-12-31 01:38:32 +0100

    Updates

---
+++ _app/pages/__layout.svelte-e54ef0b5.js
@@ -0,0 +1,35 @@
+import{S as V,i as Z,s as q,y as C,e as l,g as d,t as H,a as t,z as s,b as h,A as M,B as T,C as A,q as E,n as F,d as n}from"../chunks/vendor-8899c02f.js";import{b as S}from"../chunks/paths-28a87002.js";function G(y){let o,f,i,p,v,c,K,L,_,g,D,j,b,B,$,k,w,x,m,P,O,u;const z=y[1].default,a=C(z,y,y[0],null);return{c(){o=l("link"),f=d(),i=l("nav"),p=l("ul"),v=l("li"),c=l("a"),K=H("Pr\xE1ce"),L=d(),_=l("li"),g=l("a"),D=H("P\u0159idat inzer\xE1t"),j=d(),b=l("nav"),b.innerHTML=`<div class="p-2 rounded shadow bg-white"><h2 class="px-4 pt-2 pb-4 mb-2 text-xl font-bold text-csenter border-b">Kategorie</h2> 
+        <ul><li><a title="V\u0161e" href="https://superhry.vnvn.fun/" class="font-bold px-4 py-3 hover:bg-gray-100 rounded">V\u0161e</a></li> 
+			<li><a title="3D hry" href="https://superhry.vnvn.fun/3d-hry" class="px-4 py-3 hover:bg-gray-100 rounded">3D hry</a></li> 
+			<li><a title="Bojov\xE9 hry" href="https://superhry.vnvn.fun/bojove-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Bojov\xE9 hry</a></li> 
+			<li><a title="Brut\xE1ln\xED" href="https://superhry.vnvn.fun/brutalni" class="px-4 py-3 hover:bg-gray-100 rounded">Brut\xE1ln\xED</a></li> 
+			<li><a title="Hry pro dva" href="https://superhry.vnvn.fun/hry-pro-dva" class="px-4 py-3 hover:bg-gray-100 rounded">Hry pro dva</a></li> 
+			<li><a title="Karetn\xED hry" href="https://superhry.vnvn.fun/karetni" class="px-4 py-3 hover:bg-gray-100 rounded">Karetn\xED hry</a></li> 
+			<li><a title="Logick\xE9 hry" href="https://superhry.vnvn.fun/logicke-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Logick\xE9 hry</a></li> 
+			<li><a title="Oddechov\xE9" href="https://superhry.vnvn.fun/oddechove" class="px-4 py-3 hover:bg-gray-100 rounded">Oddechov\xE9</a></li> 
+			<li><a title="Post\u0159ehov\xE9" href="https://superhry.vnvn.fun/postrehove" class="px-4 py-3 hover:bg-gray-100 rounded">Post\u0159ehov\xE9</a></li> 
+			<li><a title="Pro holky" href="https://superhry.vnvn.fun/pro-holky" class="px-4 py-3 hover:bg-gray-100 rounded">Pro holky</a></li> 
+			<li><a title="Pro kluky" href="https://superhry.vnvn.fun/pro-kluky" class="px-4 py-3 hover:bg-gray-100 rounded">Pro kluky</a></li> 
+			<li><a title="Pro nejmen\u0161\xED" href="https://superhry.vnvn.fun/pro-nejmensi" class="px-4 py-3 hover:bg-gray-100 rounded">Pro nejmen\u0161\xED</a></li> 
+			<li><a title="Sk\xE1ka\u010Dky" href="https://superhry.vnvn.fun/skakacky" class="px-4 py-3 hover:bg-gray-100 rounded">Sk\xE1ka\u010Dky</a></li> 
+			<li><a title="Sportovn\xED hry" href="https://superhry.vnvn.fun/sportovni-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Sportovn\xED hry</a></li> 
+			<li><a title="Strategick\xE9" href="https://superhry.vnvn.fun/strategicke" class="px-4 py-3 hover:bg-gray-100 rounded">Strategick\xE9</a></li> 
+			<li><a title="St\u0159\xEDle\u010Dky" href="https://superhry.vnvn.fun/strilecky" class="px-4 py-3 hover:bg-gray-100 rounded">St\u0159\xEDle\u010Dky</a></li> 
+			<li><a title="Z\xE1vodn\xED hry" href="https://superhry.vnvn.fun/zavodni-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Z\xE1vodn\xED hry</a></li></ul></div>`,B=d(),a&&a.c(),$=d(),k=l("nav"),k.innerHTML=`<div class="p-2 rounded shadow bg-white"><h2 class="px-4 pt-2 pb-4 mb-2 text-xl font-bold text-csenter border-b">Kategorie</h2> 
+        <ul><li><a title="V\u0161e" href="https://superhry.vnvn.fun/" class="font-bold px-4 py-3 hover:bg-gray-100 rounded">V\u0161e</a></li> 
+			<li><a title="3D hry" href="https://superhry.vnvn.fun/3d-hry" class="px-4 py-3 hover:bg-gray-100 rounded">3D hry</a></li> 
+			<li><a title="Bojov\xE9 hry" href="https://superhry.vnvn.fun/bojove-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Bojov\xE9 hry</a></li> 
+			<li><a title="Brut\xE1ln\xED" href="https://superhry.vnvn.fun/brutalni" class="px-4 py-3 hover:bg-gray-100 rounded">Brut\xE1ln\xED</a></li> 
+			<li><a title="Hry pro dva" href="https://superhry.vnvn.fun/hry-pro-dva" class="px-4 py-3 hover:bg-gray-100 rounded">Hry pro dva</a></li> 
+			<li><a title="Karetn\xED hry" href="https://superhry.vnvn.fun/karetni" class="px-4 py-3 hover:bg-gray-100 rounded">Karetn\xED hry</a></li> 
+			<li><a title="Logick\xE9 hry" href="https://superhry.vnvn.fun/logicke-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Logick\xE9 hry</a></li> 
+			<li><a title="Oddechov\xE9" href="https://superhry.vnvn.fun/oddechove" class="px-4 py-3 hover:bg-gray-100 rounded">Oddechov\xE9</a></li> 
+			<li><a title="Post\u0159ehov\xE9" href="https://superhry.vnvn.fun/postrehove" class="px-4 py-3 hover:bg-gray-100 rounded">Post\u0159ehov\xE9</a></li> 
+			<li><a title="Pro holky" href="https://superhry.vnvn.fun/pro-holky" class="px-4 py-3 hover:bg-gray-100 rounded">Pro holky</a></li> 
+			<li><a title="Pro kluky" href="https://superhry.vnvn.fun/pro-kluky" class="px-4 py-3 hover:bg-gray-100 rounded">Pro kluky</a></li> 
+			<li><a title="Pro nejmen\u0161\xED" href="https://superhry.vnvn.fun/pro-nejmensi" class="px-4 py-3 hover:bg-gray-100 rounded">Pro nejmen\u0161\xED</a></li> 
+			<li><a title="Sk\xE1ka\u010Dky" href="https://superhry.vnvn.fun/skakacky" class="px-4 py-3 hover:bg-gray-100 rounded">Sk\xE1ka\u010Dky</a></li> 
+			<li><a title="Sportovn\xED hry" href="https://superhry.vnvn.fun/sportovni-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Sportovn\xED hry</a></li> 
+			<li><a title="Strategick\xE9" href="https://superhry.vnvn.fun/strategicke" class="px-4 py-3 hover:bg-gray-100 rounded">Strategick\xE9</a></li> 
+			<li><a title="St\u0159\xEDle\u010Dky" href="https://superhry.vnvn.fun/strilecky" class="px-4 py-3 hover:bg-gray-100 rounded">St\u0159\xEDle\u010Dky</a></li> 
+			<li><a title="Z\xE1vodn\xED hry" href="https://superhry.vnvn.fun/zavodni-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Z\xE1vodn\xED hry</a></li></ul></div>`,w=d(),x=l("footer"),m=l("div"),P=l("a"),O=H("inzerce.github.io"),t(o,"rel","icon"),t(o,"href",""+(S+"/favicon.png")),t(c,"href",""+(S+"/")),t(c,"class","p-3 font-bold uppercase text-white"),t(g,"href",""+(S+"/pridat-inzerat")),t(g,"class","p-3 text-slate-100"),t(p,"class","container p-2 flex justify-between"),t(i,"class","fixed top-0 inset-x-0 z-50 shadow bg-blue-800"),t(b,"class","flex flex-col gap-2 lg:gap-4"),t(k,"class","flex flex-col gap-2 lg:gap-4"),t(P,"href",""+(S+"/")),t(m,"class","container p-5 pt-12 text-center"),t(x,"class","")},m(e,r){s(document.head,o),h(e,f,r),h(e,i,r),s(i,p),s(p,v),s(v,c),s(c,K),s(p,L),s(p,_),s(_,g),s(g,D),h(e,j,r),h(e,b,r),h(e,B,r),a&&a.m(e,r),h(e,$,r),h(e,k,r),h(e,w,r),h(e,x,r),s(x,m),s(m,P),s(P,O),u=!0},p(e,[r]){a&&a.p&&(!u||r&1)&&M(a,z,e,e[0],u?A(z,e[0],r,null):T(e[0]),null)},i(e){u||(E(a,e),u=!0)},o(e){F(a,e),u=!1},d(e){n(o),e&&n(f),e&&n(i),e&&n(j),e&&n(b),e&&n(B),a&&a.d(e),e&&n($),e&&n(k),e&&n(w),e&&n(x)}}}function I(y,o,f){let{$$slots:i={},$$scope:p}=o;return y.$$set=v=>{"$$scope"in v&&f(0,p=v.$$scope)},[p,i]}class Q extends V{constructor(o){super();Z(this,o,I,G,q,{})}}export{Q as default};
---
+++ _app/pages/pridat-inzerat.svelte-5e9f976d.js
@@ -0,0 +1,18 @@
+import{S as s,i as h,s as l,e as o,a as p,b as y,D as t,d as i}from"../chunks/vendor-8899c02f.js";function v(a){let r;return{c(){r=o("nav"),r.innerHTML=`<div class="p-2 rounded shadow bg-white"><h2 class="px-4 pt-2 pb-4 mb-2 text-xl font-bold text-csenter border-b">Kategorie</h2> 
+        <ul><li><a title="V\u0161e" href="https://superhry.vnvn.fun/" class="font-bold px-4 py-3 hover:bg-gray-100 rounded">V\u0161e</a></li> 
+			<li><a title="3D hry" href="https://superhry.vnvn.fun/3d-hry" class="px-4 py-3 hover:bg-gray-100 rounded">3D hry</a></li> 
+			<li><a title="Bojov\xE9 hry" href="https://superhry.vnvn.fun/bojove-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Bojov\xE9 hry</a></li> 
+			<li><a title="Brut\xE1ln\xED" href="https://superhry.vnvn.fun/brutalni" class="px-4 py-3 hover:bg-gray-100 rounded">Brut\xE1ln\xED</a></li> 
+			<li><a title="Hry pro dva" href="https://superhry.vnvn.fun/hry-pro-dva" class="px-4 py-3 hover:bg-gray-100 rounded">Hry pro dva</a></li> 
+			<li><a title="Karetn\xED hry" href="https://superhry.vnvn.fun/karetni" class="px-4 py-3 hover:bg-gray-100 rounded">Karetn\xED hry</a></li> 
+			<li><a title="Logick\xE9 hry" href="https://superhry.vnvn.fun/logicke-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Logick\xE9 hry</a></li> 
+			<li><a title="Oddechov\xE9" href="https://superhry.vnvn.fun/oddechove" class="px-4 py-3 hover:bg-gray-100 rounded">Oddechov\xE9</a></li> 
+			<li><a title="Post\u0159ehov\xE9" href="https://superhry.vnvn.fun/postrehove" class="px-4 py-3 hover:bg-gray-100 rounded">Post\u0159ehov\xE9</a></li> 
+			<li><a title="Pro holky" href="https://superhry.vnvn.fun/pro-holky" class="px-4 py-3 hover:bg-gray-100 rounded">Pro holky</a></li> 
+			<li><a title="Pro kluky" href="https://superhry.vnvn.fun/pro-kluky" class="px-4 py-3 hover:bg-gray-100 rounded">Pro kluky</a></li> 
+			<li><a title="Pro nejmen\u0161\xED" href="https://superhry.vnvn.fun/pro-nejmensi" class="px-4 py-3 hover:bg-gray-100 rounded">Pro nejmen\u0161\xED</a></li> 
+			<li><a title="Sk\xE1ka\u010Dky" href="https://superhry.vnvn.fun/skakacky" class="px-4 py-3 hover:bg-gray-100 rounded">Sk\xE1ka\u010Dky</a></li> 
+			<li><a title="Sportovn\xED hry" href="https://superhry.vnvn.fun/sportovni-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Sportovn\xED hry</a></li> 
+			<li><a title="Strategick\xE9" href="https://superhry.vnvn.fun/strategicke" class="px-4 py-3 hover:bg-gray-100 rounded">Strategick\xE9</a></li> 
+			<li><a title="St\u0159\xEDle\u010Dky" href="https://superhry.vnvn.fun/strilecky" class="px-4 py-3 hover:bg-gray-100 rounded">St\u0159\xEDle\u010Dky</a></li> 
+			<li><a title="Z\xE1vodn\xED hry" href="https://superhry.vnvn.fun/zavodni-hry" class="px-4 py-3 hover:bg-gray-100 rounded">Z\xE1vodn\xED hry</a></li></ul></div>`,p(r,"class","flex flex-col gap-2 lg:gap-4")},m(e,n){y(e,r,n)},p:t,i:t,o:t,d(e){e&&i(r)}}}class d extends s{constructor(r){super();h(this,r,null,v,l,{})}}export{d as default};
